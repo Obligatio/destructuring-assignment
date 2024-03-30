53167910de9c521df820a7d23e95e4d91d6cb634
kevin <kevinke386@gmail.com>
2024-03-31 00:39:43 +0300

    destructuring assignment

---
+++ index.js
@@ -0,0 +1,25 @@
+const farmAnimals = 'Goat turkey sheep cow duck';
+
+const colors = ['black', 'white', 'brown', 'green', 'yellow', 'red', 'purple'];
+
+const animal = {
+    animalName: 'The GOAT',
+    color: 'black',
+    age: '12',
+    offspring: 'turk'
+};
+
+const nestedanimal = {
+    nestedName: 'turk',
+    nestedColor: 'brown',
+    nestedoffspring: {
+        thefarmfamily: {
+            offspring1: 'ghazaal',
+            offspring2: 'dian',
+        },
+    },
+    nestedJob: 'Protection',
+    nestedPartner: 'The GOAT'
+};
+
+// Strings
